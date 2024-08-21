7f1111a0fb2573bda085df16e9a9b631c5e3ec4e
mhmedels <me9299962@gmail.com>
2024-08-21 16:58:27 +0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,12 @@
+//add class active to header on scroll
+
+let header = document.querySelector("header")
+
+window.onscroll = function(){
+    if(this.scrollY >=50){
+        header.classList.add("active")
+    }
+    else{
+        header.classList.remove("active")
+    }
+}
